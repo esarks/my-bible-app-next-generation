538971b517e86d12aa9d0233d1ee53d80783c636
Paul Marshall <ptm@esarks.com>
2025-07-03 08:50:02 -0400

    Update send-code.js

--- backend/routes/send-code.js
+++ backend/routes/send-code.js
@@ -3,13 +3,18 @@ const express = require("express");
 const router = express.Router();
 const twilio = require("twilio");
 
+//TWILIO_VERIFY=VAdd281d2f09c45e85e9ad9466b2ef3929
+//TWILIO_SID=ACba43e938d7270668a6c8758b35a362db
+//TWILIO_AUTH_TOKEN=4aacb429a7d0105a48cee5ce5092a993
+//TWILIO_PHONE_NUMBER=+17275917765
+
 const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const serviceSid = process.env.TWILIO_SERVICE_SID;
+const serviceSid = process.env.TWILIO_VERIFY;
 
 console.log("[DEBUG] TWILIO_SID:", process.env.TWILIO_SID);
 console.log("[DEBUG] TWILIO_AUTH_TOKEN:", process.env.TWILIO_AUTH_TOKEN);
-console.log("[DEBUG] TWILIO_SERVICE_SID:", process.env.TWILIO_SERVICE_SID);
+console.log("[DEBUG] TWILIO_SERVICE_SID:", process.env.TWILIO_VERIFY);
 
 const client = twilio(accountSid, authToken);
 
