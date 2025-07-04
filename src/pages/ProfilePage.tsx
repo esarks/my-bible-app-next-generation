6d3e90cb5ec306409d517980441057704b6ac2a1
Paul Marshall <ptm@esarks.com>
2025-07-04 11:20:52 -0400

    fix: remove redundant layout wrapper

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import PageLayoutWrapper from "../components/PageLayoutWrapper";
 import Profile from "../components/Profile";
 
 export default function ProfilePage() {
   return (
-    <PageLayoutWrapper>
-      <Profile />
-    </PageLayoutWrapper>
+    <Profile />
   );
 }
